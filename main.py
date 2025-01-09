d9f9f9e6ede45071687cb648abc761c8f954a436
alisa-01-rai <alisa.rai333@gmail.com>
2025-01-09 07:44:41 -0800

    function inside function

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from tkinter import *
 from tkinter import Canvas, PhotoImage
+
+import pandas
 import pandas as pd
 import random
 
+from pandas.io.sas.sas_constants import page_data_type
 
 #you can call function inside function to loop
 BACKGROUND_COLOR="#B1DDC6"
@@ -10,6 +13,13 @@ BACKGROUND_COLOR="#B1DDC6"
 value=pd.read_csv("data/french_words.csv")
 to_learn=value.to_dict(orient="records")
 current_card={}
+
+try:
+   data= pandas.read_csv("data/french_words.csv")
+except FileNotFoundError:
+    original_data=pandas.read_csv
+
+    to_learn=original_data.to_dict(orient="records")
 def next_card():
     global current_card
     current_card=random.choice(to_learn)
@@ -27,7 +37,11 @@ def flip_card():
     canvas.itemconfig(card_title, text="english")
     canvas.itemconfig(card_word, text=current_card["English"])
 
-
+def is_checked():
+    to_learn.remove(current_card)
+    print(len(to_learn))
+    data=pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False)
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
